abf41115fb374801abed6ee568a3ff193fb5ef7b
ImCitizen13 <meltohamy@protonmail.com>
2024-03-21 14:55:33 +0400

    Updated CORS in next config

--- next.config.js
+++ next.config.js
@@ -16,23 +16,44 @@ const config = {
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
-  }, 
+  },
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: 'ord-mirror.magiceden.dev'
-      }, 
-      {
-        protocol: "https",
-        hostname: "renderer.magiceden.dev"
+        hostname: "ord-mirror.magiceden.dev",
       },
       {
         protocol: "https",
-        hostname: "ordin-delta.vercel.app"
-      }
-    ]
-  }
+        hostname: "renderer.magiceden.dev",
+      },
+      {
+        protocol: "https",
+        hostname: "ordin-delta.vercel.app",
+      },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default config;
